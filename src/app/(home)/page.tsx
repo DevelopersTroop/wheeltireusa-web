b7abaf2097697a2a64cee2e60e52054e67ef88ad
Habibul Islam <habibulislam6862@gmail.com>
2025-04-22 17:47:24 +0600

    updated home

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,10 @@
-import { HomeHero } from "./_components/hero";
+import { HomeHero } from './_components/hero';
 
-export default function Homepage(){
-    return(
-        <div>
-            <HomeHero/>
-        </div>
-    )
+export default function Homepage() {
+  return (
+    <div>
+      hello world
+      <HomeHero />
+    </div>
+  );
 }
