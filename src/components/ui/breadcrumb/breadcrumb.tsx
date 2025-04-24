2f99a7452a3cfe7a3832a3ee245c352fd85fdce9
SujonHasan <sujonhasan171@gmail.com>
2025-04-24 22:13:59 +0600

    single product page design

---
+++ src/components/ui/breadcrumb/breadcrumb.tsx
@@ -0,0 +1,23 @@
+// import React from 'react';
+
+// const Breadcrumb = ({children}: {children: React.ReactNode}) => {
+//     return (
+//         <ol className="flex px-2 items-center whitespace-nowrap flex-wrap gap-x-9 gap-y-1.5">
+//             {children}
+//         </ol>
+//     );
+// };
+
+// export default Breadcrumb;
+
+import React from 'react';
+
+const Breadcrumb = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <ol className="flex items-center text-wrap flex-wrap gap-x-1 gap-y-1.5">
+      {children}
+    </ol>
+  );
+};
+
+export default Breadcrumb;
---
+++ src/components/ui/quantity-input-box/cart-quantity-input-box.tsx
@@ -0,0 +1,85 @@
+'use client';
+import { useTypedSelector } from '@/app/globalRedux/store';
+import { Minus, Plus } from 'lucide-react';
+import React, { useMemo } from 'react';
+
+type QuantityInputBoxProps = {
+  className?: string;
+  onIncrease: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  onDecrease: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  onInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  inputValue: string | number;
+  quantityStep: string | number;
+  maxInputValue: string | number;
+  inputName: string;
+  id: string;
+  borderColor?: string;
+};
+const CartQuantityInputBox = ({
+  onIncrease,
+  onDecrease,
+  inputName,
+  id,
+  onInputChange,
+  inputValue,
+  quantityStep,
+  maxInputValue,
+}: QuantityInputBoxProps) => {
+  const { products } = useTypedSelector((state) => state.persisted.cart);
+
+  const isDisabled = useMemo(() => {
+    const productsData = Object.values(products).filter(
+      (p) => p.cartPackage === id && p.item_class !== 'Accessories'
+    );
+    const increseDisabled = productsData.some((p) => {
+      return (
+        p.inventory_available &&
+        (p?.quantity ?? 1) + (p?.inventoryStep ?? 1) - 1 >=
+          p.inventory_available
+      );
+    });
+    const decreaseDisabled = productsData.some(
+      (p) => p.quantity === p.minInventory
+    );
+    return {
+      increase: increseDisabled,
+      decrease: decreaseDisabled,
+    };
+  }, [Object.values(products), id]);
+  return (
+    <div className={'flex gap-0 items-start relative w-[164px]'}>
+      <button
+        onClick={onIncrease}
+        disabled={isDisabled.decrease}
+        className="rounded-tl-xl rounded-bl-xl border border-[#cfcfcf] p-3 flex gap-2 justify-center items-center relative w-14 h-14 bg-white disabled:cursor-not-allowed disabled:text-gray-500"
+      >
+        <Minus size={18} />
+      </button>
+      <div className="border-x-0 border-y border-[#cfcfcf] p-3 flex gap-2 justify-center items-center flex-1 self-stretch relative w-full bg-white">
+        <input
+          disabled
+          onChange={onInputChange}
+          value={inputValue}
+          step={quantityStep}
+          min={quantityStep}
+          max={maxInputValue}
+          type="number"
+          name={inputName}
+          id={id}
+          className={
+            '-mt-0.5 h-full text-center appearance-none-input-number focus:outline-none disabled:cursor-not-allowed bg-white'
+          }
+        />
+      </div>
+      <button
+        disabled={isDisabled.increase}
+        onClick={onDecrease}
+        className="rounded-tr-xl rounded-br-xl border border-[#cfcfcf] p-3 flex gap-2 justify-center items-center relative w-14 h-14 bg-white disabled:cursor-not-allowed disabled:text-gray-500"
+      >
+        <Plus size={18} />
+      </button>
+    </div>
+  );
+};
+
+export default CartQuantityInputBox;
