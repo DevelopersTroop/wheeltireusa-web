7d1aa822c253953665ce4be4480e8ada677d5027
Borhan Uddin <borhan.dev@gmail.com>
2025-05-31 20:12:26 +0600

    feat: pm2 ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: 'tiremetic-stage-web',
+      script: 'npm',
+      args: 'start',
+    },
+    {
+      name: 'tiremetic-web',
+      script: 'npm',
+      args: 'start',
+    },
+  ],
+};
