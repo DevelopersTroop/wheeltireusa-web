7eaa6ddcf4baed8d94d5f13fcc015bf93fd9f700
Jahid Hiron <namehiron.96@gmail.com>
2025-05-31 21:58:29 +0600

    ISSUE: deployment

--- nginx/stage-setup.sh
+++ nginx/stage-setup.sh
@@ -19,20 +19,20 @@ server {
     set \$commit_sha $COMMIT_SHA;
 
     location / {
-        proxy_pass http://localhost:3000;
+        proxy_pass http://localhost:3001;
         proxy_http_version 1.1;
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto $scheme;
-        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+        proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection 'upgrade';
-        proxy_cache_bypass $http_upgrade;
+        proxy_cache_bypass \$http_upgrade;
         add_header Cache-Control "no-cache, no-store, must-revalidate";
     }
 
-    location ~ ^/_next/static/(.*)$ {
-        proxy_pass http://dpgcl9gapwcdl.cloudfront.net/oe-web/$commit_sha/_next/static/$1;
+    location ~ ^/_next/static/(.*)\$ {
+        proxy_pass http://dpgcl9gapwcdl.cloudfront.net/oe-web/\$commit_sha/_next/static/\$1;
         proxy_set_header Host dpgcl9gapwcdl.cloudfront.net;
         proxy_ssl_name dpgcl9gapwcdl.cloudfront.net;
         proxy_ssl_server_name on;
@@ -42,12 +42,12 @@ server {
     }
 
     location @fallback_next_static {
-        proxy_pass http://localhost:3000/_next/static/$1;
-        proxy_set_header Host $host;
+        proxy_pass http://localhost:3001/_next/static/\$1;
+        proxy_set_header Host \$host;
     }
 
-    location ~ ^/public/(.*)$ {
-        proxy_pass http://dpgcl9gapwcdl.cloudfront.net/oe-web/$commit_sha/public/$1;
+    location ~ ^/public/(.*)\$ {
+        proxy_pass http://dpgcl9gapwcdl.cloudfront.net/oe-web/\$commit_sha/public/\$1;
         proxy_set_header Host dpgcl9gapwcdl.cloudfront.net;
         proxy_ssl_name dpgcl9gapwcdl.cloudfront.net;
         proxy_ssl_server_name on;
@@ -57,8 +57,8 @@ server {
     }
 
     location @fallback_public {
-        proxy_pass http://localhost:3000/public/$1;
-        proxy_set_header Host $host;
+        proxy_pass http://localhost:3001/public/\$1;
+        proxy_set_header Host \$host;
     }
 
     location /healthz {
@@ -66,25 +66,21 @@ server {
         add_header Content-Type text/plain;
     }
 
-
     listen 443 ssl; # managed by Certbot
     ssl_certificate /etc/letsencrypt/live/tiremetic.stage.developertroop.com/fullchain.pem; # managed by Certbot
     ssl_certificate_key /etc/letsencrypt/live/tiremetic.stage.developertroop.com/privkey.pem; # managed by Certbot
     include /etc/letsencrypt/options-ssl-nginx.conf; # managed by Certbot
     ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem; # managed by Certbot
-
 }
-server {
-    if ($host = tiremetic.stage.developertroop.com) {
-        return 301 https://$host$request_uri;
-    } # managed by Certbot
 
+server {
+    if (\$host = tiremetic.stage.developertroop.com) {
+        return 301 https://\$host\$request_uri;
+    } # managed by Certbot
 
     server_name tiremetic.stage.developertroop.com;
     listen 80;
     return 404; # managed by Certbot
-
-
 }
 EOF
 
