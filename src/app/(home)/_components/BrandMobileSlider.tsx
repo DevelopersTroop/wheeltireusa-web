3382a043000c78634c7142cb9f1225affc060e4b
Borhan Uddin <borhan.dev@gmail.com>
2025-06-18 20:57:05 +0600

    feat: Homepage Tire Best brand section

--- src/app/(home)/_components/BrandMobileSlider.tsx
+++ src/app/(home)/_components/BrandMobileSlider.tsx
@@ -1,12 +1,15 @@
 'use client';
-import { useEffect, useRef } from 'react';
+import { useScreenSize } from '@/hooks/useScreenSize';
+import { useEffect, useRef, useState } from 'react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import { Navigation } from 'swiper/modules';
 import { Swiper, SwiperRef, SwiperSlide } from 'swiper/react';
 
-const BrandSlider = ({ images }: { images: string[] }) => {
+export const BrandMobileSlider = ({ images }: { images: string[] }) => {
   const swiperRef = useRef<SwiperRef>(null);
+  const [screenWidth, setScreenWidth] = useState('16%');
+  const { screenSize } = useScreenSize();
 
   useEffect(() => {
     const timeOut = setTimeout(() => {
@@ -16,21 +19,35 @@ const BrandSlider = ({ images }: { images: string[] }) => {
     return () => clearTimeout(timeOut);
   }, []);
 
+  useEffect(() => {
+    if (screenSize > 768) {
+      setScreenWidth('16%');
+    } else if (screenSize > 650) {
+      setScreenWidth('23%');
+    } else if (screenSize > 450) {
+      setScreenWidth('28%');
+    } else {
+      setScreenWidth('34%');
+    }
+  }, [screenSize]);
+
   return (
     <div>
       <Swiper
         ref={swiperRef}
         modules={[Navigation]}
-        slidesPerView={3}
+        slidesPerView={'auto'}
         spaceBetween={16}
         loop
         onSlideChange={(swiper) => {}}
-        // onScroll={(swiper) => {
-        //   swiper.slideNext()
-        // }}
+        centeredSlides
       >
         {images.map((img, index) => (
-          <SwiperSlide key={index} className="w-full">
+          <SwiperSlide
+            key={index}
+            className="w-full"
+            style={{ width: screenWidth }}
+          >
             <div className="flex justify-center items-center w-full h-full cursor-pointer rounded-[10px] overflow-hidden hover:scale-110 duration-300 transition-transform">
               <img src={img} alt="image" className="w-full h-full" />
             </div>
@@ -40,5 +57,3 @@ const BrandSlider = ({ images }: { images: string[] }) => {
     </div>
   );
 };
-
-export default BrandSlider;
