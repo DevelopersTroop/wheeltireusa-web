0a98975956d3eef405823c52163a42770989b245
SujonHasan <sujonhasan171@gmail.com>
2025-08-03 16:34:15 +0600

    auth issue fixed

--- src/app/(auth)/auth/verify-email/_components/EmailVerification/useEmailVerification.ts
+++ src/app/(auth)/auth/verify-email/_components/EmailVerification/useEmailVerification.ts
@@ -16,7 +16,7 @@ export const useEmailVerification = () => {
         const token = searchParams.get('token');
         const email = searchParams.get('email');
 
-        const response = await fetch(`${apiBaseUrl}/auth/email-verification`, {
+        const response = await fetch(`${apiBaseUrl}/auth/verify-email`, {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({ token, email }),
