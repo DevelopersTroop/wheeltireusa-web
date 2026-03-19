f831fcb9b985cf714da5797eb15246e6e8ebd0e7
Nahid Hossain <nahid4510@gmail.com>
2026-03-19 17:30:40 +0600

    done

--- src/components/shared/Header/components/MiddleHeader/MiddleHeader.tsx
+++ src/components/shared/Header/components/MiddleHeader/MiddleHeader.tsx
@@ -44,7 +44,7 @@ export default function MiddleHeader() {
         )}
 
         <div className="py-3">
-          <div className="flex items-center justify-between gap-4">
+          <div className="flex items-center justify-between gap-0">
             <MobileNavbar />
 
             <div className="flex items-center overflow-hidden shrink-0">
